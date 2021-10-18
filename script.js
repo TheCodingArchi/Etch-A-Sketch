5c83e7f0fc43ad2205c3fb863871db3cba557807
Tosin Akinlua <akinluarotimi@gmail.com>
2021-10-18 23:36:26 +0100

    Implement erase function to erase drawing progressively, clearDrawing function to delete entire drawing at once and getRandomColors function to draw with random colors

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ let gridSize = 16;
 const gridContainer = document.querySelector('.grid-container');
 const changeGridSizeBtn = document.querySelector('button.change-grid-size');
 const form = document.querySelector('form');
+const eraser = document.querySelector('button.eraser');
+const clearSketchBtn = document.querySelector('button.clear-sketch');
+
 
 document.addEventListener('DOMContentLoaded', createGrid);
 changeGridSizeBtn.addEventListener('click', displayInputBox);
 form.addEventListener('submit', setNewGridSize);
-
+eraser.addEventListener('click', erase);
+clearSketchBtn.addEventListener('click', clearDrawing);
 
 function createGrid() {
     for (let i = 1; i <= gridSize ** 2; i++) {
         const grid = document.createElement('div');
         grid.style.height = `calc(100% / ${gridSize})`;
         grid.style.width = `calc(100% / ${gridSize})`;
-        grid.style.border = '1px solid grey';
+        grid.style.border = '1px solid #e4e4e4';
         gridContainer.appendChild(grid);
     }
     draw();
@@ -43,10 +47,32 @@ function setNewGridSize(event) {
 function draw() {
     const rows = document.querySelectorAll('.grid-container > div');
     rows.forEach(row => {
-        row.addEventListener('mouseover', () => {
-            row.style.backgroundColor = '#000000';
+        row.addEventListener('mouseenter', () => {
+            row.style.backgroundColor = getRandomColors();
         });
-    })
+    });
 }
 
+function clearDrawing() {
+    const rows = document.querySelectorAll('.grid-container > div');
+    rows.forEach(row => {
+        row.style.backgroundColor = 'initial';
+    });
+}
+
+function erase() {
+    const rows = document.querySelectorAll('.grid-container > div');
+    rows.forEach(row => {
+        row.addEventListener('mouseenter', () => {
+            row.style.backgroundColor = 'initial';
+        });
+    });
+}
+
+function getRandomColors() {
+    const randomColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    const randomNumber = (Math.floor(Math.random() * 10)) % 7;
+    const randomColor = randomColors[randomNumber];
+    return randomColor;
+}
 
